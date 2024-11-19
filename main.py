1a0b1d8882513070e62332816368675957985b13
raunak <raunakbiswas05@gmail.com>
2024-11-19 11:59:28 +0530

    Updated to do list with complete.

--- main.py
+++ main.py
@@ -6,22 +6,26 @@ string.upper()
 
 
 while 3 > num:
-    text = input("Add, Show, Edit or End: ")
+    text = input("Add, Show, Edit, Complete or End: ")
     text = text.strip().lower()
     match text:
         case "add":
             toDo = input("enter your toDo: ")
             items.append(toDo)
         case "show":
-            number = 1
-            for item in items:
-                print(str(num)+".",item)
-                num+=1
+            # for index, item in items:
+            for index,item in enumerate(items):
+                print(f"{index+1}. {item}")
+
         case "edit":
-            editNumber = int(input("number of the toDo to edit: "))
+            number = int(input("number of the toDo to edit: "))
             newTodo = input("enter what edited todo: ")
-            items[editNumber-1] = newTodo
+            items[number-1] = newTodo
             print()
+        case "complete":
+            number = int(input("enter the number of ToDo to complete: "))
+            items.remove(items[number-1]) #can also use pop with index number
+
         case "end":
             break
         case somethingElse:
---
+++ text.py
@@ -0,0 +1,3 @@
+seconds = [1.23, 1.45, 1.02, 1.11]
+seconds.remove(seconds[1])
+print(seconds)
